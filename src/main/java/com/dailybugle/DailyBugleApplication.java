ae0174e8959dd42905ec6f49dc7d0642e3016116
Ezz4 <zuryta92@gmail.com>
2021-04-20 21:33:45 -0500

    Starting for index for the daily bugle

---
+++ src/main/java/com/dailybugle/DailyBugleApplication.java
@@ -0,0 +1,13 @@
+package com.dailybugle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyBugleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyBugleApplication.class, args);
+	}
+
+}
